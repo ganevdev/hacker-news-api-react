a9f701ead9ee5770a70c113eaa210984edc177e9
Ivan Ganev <iganevru@gmail.com>
2020-01-01 09:32:23 +0300

    work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import './App.css';
 
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-  return (
-    <div className="App">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
-};
+const API = 'https://hn.algolia.com/api/v1/search?query=';
+const DEFAULT_QUERY = 'redux';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hits: [],
+      isLoading: false,
+      error: null,
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+
+    try {
+      let result = await fetch(API + DEFAULT_QUERY);
+      result = await result.json();
+      this.setState({
+        hits: result.hits,
+        isLoading: false,
+      });
+    } catch (error) {
+      this.setState({
+        error,
+        isLoading: false,
+      });
+    }
+  }
+
+  render() {
+    const { hits, isLoading, error } = this.state;
+
+    if (error) return <p>{error.message}</p>;
+    if (isLoading) return <p>Loading...</p>;
+    return (
+      <ul>
+        {hits.map(hit => (
+          <li key={hit.objectID}>
+            <a href={hit.url}>{hit.title}</a>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
 export default App;
